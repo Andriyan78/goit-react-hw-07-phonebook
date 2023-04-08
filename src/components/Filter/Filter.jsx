b5c5c6437f0a0cfe58543abd072497bccf4a3fcf
Andriyan78 <110392316+Andriyan78@users.noreply.github.com>
2023-04-08 20:41:10 +0200

    first

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { Label, Input } from './Filter.styled';
+import { setFilter } from 'redux/filterSlice';
+
+
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const handleFilter = ({ target: { value } }) => {
+    dispatch(setFilter(value));
+  };
+  return (
+    <Label>
+      Find contacts by name
+      <Input name="filter" type="text" onChange={handleFilter} />
+    </Label>
+  );
+};
